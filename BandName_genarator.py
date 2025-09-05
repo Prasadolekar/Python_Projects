ab4c6da6c99d69fb02a8d8d67958d1b8333e8434
Prasad Prakash Olekar <prasadpolekar8971@gamil.com>
2025-09-06 00:43:51 +0530

    Added BandName Generator project

---
+++ BandName_genarator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+a=input("What's the name of the city you grew up in?\n")
+b=input("What's your pet name?.\n")
+print("Your band name could: " + a +' '+ b)
